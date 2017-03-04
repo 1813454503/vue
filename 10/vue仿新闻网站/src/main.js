8f0f759f49aa1763f56e983910e3d17fedec9567
cksfdd <cksfdd@sina.com>
2017-03-04 18:51:35 +0800

    *

--- 10/vue仿新闻网站/src/main.js
+++ 10/vue仿新闻网站/src/main.js
@@ -9,6 +9,8 @@ import routes from './routeConfig.js'
 
 import filters from './filters'
 
+//Vue.filter(名字,函数)
+//循环遍历所有过滤器
 Object.keys(filters).forEach(key => Vue.filter(key, filters[key]))
 
 Vue.use(VueRouter);
@@ -16,37 +18,37 @@ Vue.use(Loading);
 
 require('./assets/css/base.css'); //引入全局的base文件
 
-const router=new VueRouter({
-	mode: 'history', //切换路径模式，变成history模式
-  scrollBehavior: () => ({ y: 0 }), // 滚动条滚动的行为，不加这个默认就会记忆原来滚动条的位置
-	routes
+const router = new VueRouter({
+    mode: 'history', //切换路径模式，变成history模式
+    scrollBehavior: () => ({y: 0}), // 滚动条滚动的行为，不加这个默认就会记忆原来滚动条的位置
+    routes
 });
 
 //axios的一些配置，比如发送请求显示loading，请求回来loading消失之类的
-//
+
 axios.interceptors.request.use(function (config) {  //配置发送请求的信息
-  stores.dispatch('showLoading')  
-  return config;
+    stores.dispatch('showLoading')
+    return config;
 }, function (error) {
-  return Promise.reject(error);
+    return Promise.reject(error);
 });
 
 axios.interceptors.response.use(function (response) { //配置请求回来的信息
-  stores.dispatch('hideLoading')
-  return response;
+    stores.dispatch('hideLoading')
+    return response;
 }, function (error) {
 
-  return Promise.reject(error);
+    return Promise.reject(error);
 });
 
-/*axios.defaults.baseURL = (process.env.NODE_ENV !=='production' ? config.dev.httpUrl:config.build.httpUrl);
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';*/
-//axios.defaults.baseURL='http://localhost:8082/';
-Vue.prototype.$http = axios  //其他页面在使用axios的时候直接  this.$http就可以了
+// axios.defaults.baseURL = (process.env.NODE_ENV !== 'production' ? config.dev.httpUrl : config.build.httpUrl);//配置请求根路径
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';//设置post头部信息
+
+Vue.prototype.$http = axios;  //把axios对象挂到Vue原型上,其他页面在使用axios的时候直接->this.$http就可以了
 
 new Vue({
-  el: '#app',
-  router,
-  store:stores,
-  render: h => h(App)
+    el: '#app',
+    router,
+    store: stores,
+    render: h => h(App)
 })
