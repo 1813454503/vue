971fe93eb31a62d2f36e071c2054427f9876fff4
cksfdd <cksfdd@sina.com>
2017-03-09 14:39:22 +0800

    *

--- 10/vue-news/webpack.config.js
+++ 10/vue-news/webpack.config.js
@@ -9,42 +9,50 @@ module.exports = {
     filename: 'build.js'
   },
   module: {
-    rules: [{
-      test: /\.vue$/,
-      loader: 'vue-loader',
-      options: {
-        // vue-loader options go here
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          // other vue-loader options go here
+        }
+      },
+      {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
       }
-    }, {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    }, {
-      test: /\.(png|jpg|gif|svg)$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]?[hash]'
-      }
-    }]
+    ]
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.common.js'
+      'vue$': 'vue/dist/vue.esm.js'
     }
   },
   devServer: {
     historyApiFallback: true,
     noInfo: true
   },
+  performance: {
+    hints: false
+  },
   devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
-    // http://vue-loader.vuejs.org/en/workflow/production.html
+  // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
